d8c7bde473026f4b4ccb8c217a77f04e5c38aec8
Anthony Barragan <anthony.bar.89@gmail.com>
2022-04-23 12:10:25 -0700

    thoughts are deleted when user are deleted

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -85,29 +85,17 @@ const userController = {
             res.status(400).json(error)
         })
     },
-    // deleteUser({ params }, res) {
-    //     User.findByIdAndDelete({ _id: params.userId })
-    //         .then(userData => {
-    //             if(!userData) {
-    //                 res.status(404).json({ message: 'No user found with that ID.'});
-    //                 return;
-    //             }
-    //             console.log(userData)
-    //             res.json(userData)
-    //         })
-    //         .catch(err => {
-    //             res.status(500).json(err)
-    //         });
-    // }
     deleteUser({ params }, res) {
-        User.findByIdAndRemove({ _id: params.userId })
+        User.findByIdAndDelete({ _id: params.userId })
             .then(userData => {
                 if(!userData) {
                     res.status(404).json({ message: 'No user found with that ID.'});
                     return;
                 }
-                console.log(userData)
-                res.json(userData)
+                return Thought.deleteMany({ _id: { $in: userData.thoughts } });
+            })
+            .then(() => {
+                res.json({ message: 'User and thoughts deleted.'});
             })
             .catch(err => {
                 res.status(500).json(err)
--- models/User.js
+++ models/User.js
@@ -39,27 +39,5 @@ UserSchema.virtual('friendCount').get(function() {
     return this.friends.length
 });
 
-
-/// Working on making thoughts delete when user is deleted. 
-// UserSchema.pre('remove', async function(next) {
-//     try {
-//         await Thought.remove({
-//             "_id": {
-//                 $in: this.thoughts
-//             }
-//         });
-//         next();
-//     } catch(err) {
-//         next(err)
-//     }
-// })
-// UserSchema.pre('remove', function(next) {
-//     // 'this' is the client being removed. Provide callbacks here if you want
-//     // to be notified of the calls' result.
-//     Thought.updateMany(
-//         {}
-//     )
-//     next();
-// });
 const User = model('User', UserSchema)
 module.exports = User;
